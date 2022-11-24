27ff1b0994a42091ed59325b821e5d52abf4595e
Xavier PELLE <xavierp34970@gmail.com>
2022-11-24 10:06:13 +0100

    2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+require 'faker'
+
+10.times do 
+
+    mail = Email.create(object: Faker::Lorem.sentence(word_count: 3), body: Faker::Lorem.sentence(word_count: 20))
+
+end
